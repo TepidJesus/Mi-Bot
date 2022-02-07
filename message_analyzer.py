cff1c1422e5562663ed2dfae23f1a9eca5cf7c24
Thomas Holdom <holdomthomas@gmail.com>
2022-02-07 19:05:20 +1300

    Added Message Processor
    This is the same message processor used on Xi Bot. All this does is strip the incoming masseages and turn them into a list for easy processing.

---
+++ message_analyzer.py
@@ -0,0 +1,20 @@
+class Message_processor():
+
+    def listify_message(self, message_raw):
+        print(f'[MESSAGE]: {message_raw.author} Says \"{message_raw.content}\"')
+        self.message_list = list()
+        self.message_list = message_raw.content.split(' ')
+        for i in range(len(self.message_list)):
+            if self.message_list[i].isalpha():
+                self.message_list[i] = self.message_list[i].lower()
+            elif self.message_list[i].isalnum() != True:
+                self.message_list[i] = self.message_list[i].strip('!')
+                self.message_list[i] = self.message_list[i].strip('.')
+                self.message_list[i] = self.message_list[i].strip(',')
+                self.message_list[i] = self.message_list[i].strip('-')
+                self.message_list[i] = self.message_list[i].strip('?')
+                self.message_list[i] = self.message_list[i].lower()
+            else:
+                continue
+        return self.message_list
+        
