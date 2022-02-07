bb2b69677bf305530ce8d202f926d9a0979a0726
Thomas Holdom <holdomthomas@gmail.com>
2022-02-07 19:24:46 +1300

    Added Basic Message Processing

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import discord
 from dotenv import load_dotenv
 import os
 
+from message_analyzer import Message_processor
+
+message_handler = Message_processor()
 
 
 load_dotenv()
@@ -9,12 +12,21 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 miBot = discord.Bot()
 miBot.intents.all()
 
-@miBot.slash_command()
+@miBot.slash_command() # Just A Test Command
 async def hello(ctx):
     await ctx.respond('Hello')
 
 @miBot.event
 async def on_ready():
         print(f'[INFO] Mi Bot Has Connected To Discord...')
+
+@miBot.event()
+async def on_message(message):
+    if message.authro == miBot.user:
+        return
+    else:
+        message_as_list = message_handler.listify_message(message_raw=message)
+
+
 
 miBot.run(TOKEN)
