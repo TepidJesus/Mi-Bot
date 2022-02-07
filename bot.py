044878b93352b8b41e012d324519017d949a23b1
Thomas Holdom <holdomthomas@gmail.com>
2022-02-07 18:58:14 +1300

    Added Intents

--- bot.py
+++ bot.py
@@ -1,3 +1,20 @@
 import discord
+from dotenv import load_dotenv
+import os
 
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 miBot = discord.Bot()
+miBot.intents.all()
+
+@miBot.slash_command()
+async def hello(ctx):
+    await ctx.respond('Hello')
+
+@miBot.event
+async def on_ready():
+        print(f'[INFO] Mi Bot Has Connected To Discord...')
+
+miBot.run(TOKEN)
