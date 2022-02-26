8a2629be28f0c68393120a12d20dce1b325ea2b1
Thomas Holdom <holdomthomas@gmail.com>
2022-02-26 17:19:59 +1300

    Added add_quote method
    Finished the first test variant of the method required to add quotes to the quote bank json

--- quote_keeper.py
+++ quote_keeper.py
@@ -5,10 +5,14 @@ class QuoteKeeper:
     def __init__(self):
         try:
             with open('quote_bank.json', 'x'):
-                print('[INFO] No Existing JSON Data Found\n[INFO]Generating New File')
+                print('[INFO] No Existing Quote Data Found - Generating New Quote File')
         except:
-            print('[INFO] Existing JSON Data Detected\n[INFO] A New File Will Not Be Generated')
+            print('[INFO] Existing Quote Data Detected - A New Quote File Will Not Be Generated')
 
-    def add_quote(self):
+    def add_quote(self, quote, member):
         with open('quote_bank.json', 'r') as raw_quotes:
             quote_dict = json.load(raw_quotes)
+            member_current_quotes = quote_dict[member]
+            member_current_quotes.append(quote)
+        with open('quote_bank.json', 'w') as raw_quotes:
+            json.dump(quote_dict, raw_quotes)
