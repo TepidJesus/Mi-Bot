b64939c00580b64cecccc91b0aa09bf7322ac1bd
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-07-31 11:46:40 +1200

    Migrated get_top_three function

--- modules/score_keeper.py
+++ modules/score_keeper.py
@@ -1,6 +1,7 @@
 import json
 from modules.database_manager import DataManager
 from sqlitedict import SqliteDict
+from operator import itemgetter
 
 class ScoreKeeper:
     def __init__(self, guild_members):
@@ -22,19 +23,20 @@ class ScoreKeeper:
 
     def get_top_three(self): # Top three list or if > 3 members is total num mumbers
         top_three = list()
-        with open('swear_counts.json', 'r') as raw_json_scores:
-            members_swear_counts = json.load(raw_json_scores)
-            sorted_counts = dict(sorted(members_swear_counts.items(), key=lambda item: item[1], reverse=True))
+        members_swear_counts = []
+        with SqliteDict("./data/memberData.db") as member_data:
+            for member_id in member_data.keys():
+                members_swear_counts.append((member_id, member_data[member_id]["SwearScore"]))
+        
+        members_swear_counts.sort(key=itemgetter(1), reverse=True)
 
-        lookup_range = 3
-        if len(sorted_counts) < 3:
+        if len(members_swear_counts) < 3:
             lookup_range = len(sorted_counts)
+        else:
+            lookup_range = 3
 
-        sorted_values = list(sorted_counts.values()) # Need a more effecient solution
-        sorted_keys = list(sorted_counts.keys()) # Need a more effecient solution
         for index in range(0, lookup_range):
-            temp_tup = (sorted_keys[index], sorted_values[index])
-            top_three.append(temp_tup)
+            top_three.append(members_swear_counts[index])
 
         return top_three    
 
