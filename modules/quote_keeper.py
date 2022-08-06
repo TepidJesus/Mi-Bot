866801a8b48577d8d833b96e84eda71dfb8ae652
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-08-07 09:28:01 +1200

    Refactores retrieve_quotes

--- modules/quote_keeper.py
+++ modules/quote_keeper.py
@@ -1,6 +1,10 @@
 import json
+import sqlite3
 from modules.database_manager import DataManager
 from sqlitedict import SqliteDict
+
+CLASS_KEY = "SavedQuotes"
+
 class QuoteKeeper:
 
     def __init__(self, guild_members):
@@ -13,16 +17,14 @@ class QuoteKeeper:
             for member in guild_members:
                 if member.id not in member_data.keys():
                     self.scoreKeeperDataManager.add_new_member(member)
-                    self.scoreKeeperDataManager.update_entry(member, "SavedQuotes", [])
+                    self.scoreKeeperDataManager.update_entry(member, CLASS_KEY, [])
                 else:
                     continue  
 
     def add_quote(self, quote, member):
-        self.quoteKeeperDataManager.update_entry(member, "SavedQuotes", quote, increment=True)
+        self.quoteKeeperDataManager.update_entry(member, CLASS_KEY, quote, increment=True)
         print(f"[INFO] Quote Added From {member.name}.")
 
     def retrieve_quotes(self, member):
-        with open('quote_bank.json') as raw_quotes:
-            quote_dict = json.load(raw_quotes)
-            member_quotes = quote_dict[member]
-            return member_quotes
+        with SqliteDict('./data/memberData') as member_data:
+            return member_data[member.id][CLASS_KEY]
