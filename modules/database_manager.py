81c00e59286cb81e29f15868308ad65102117737
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-08-06 13:32:25 +1200

    Fixed Instance comparison

--- modules/database_manager.py
+++ modules/database_manager.py
@@ -10,7 +10,7 @@ class DataManager:
 
     def add_new_member(self, new_member): # Adds a new member to the database (if they do not already exist.)
         with SqliteDict('./data/memberData') as member_data:
-            if new_member.isinstance(discord.member):
+            if isinstance(new_member, discord.Member):
                 new_member = new_member.id
             if new_member in member_data.keys():
                 return False
@@ -19,9 +19,9 @@ class DataManager:
             member_data.commit()
             return True
 
-    def update_entry(self, member, key, value, increment=False): #TODO: Add increment checking. If value numeric increment val, if non-numeric add to list.
+    def update_entry(self, member, key, value, increment=False):
         with SqliteDict('./data/memberData') as member_data:
-            if not member.isinstance(discord.Member):
+            if not isinstance(member, discord.Member):
                 member_id = member
             else:
                 member_id = member.id    
