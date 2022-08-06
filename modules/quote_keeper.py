9b7cb42af5ca5628aec7fca9a139b70593d92aaf
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-08-07 09:21:25 +1200

    Refactored add_quote

--- modules/quote_keeper.py
+++ modules/quote_keeper.py
@@ -8,7 +8,7 @@ class QuoteKeeper:
         self.refresh_quotes(guild_members)
 
 
-    def refresh_quotes(self, guild_members):
+    def refresh_quotes(self, guild_members): 
         with SqliteDict("./data/memberData.db") as member_data:
             for member in guild_members:
                 if member.id not in member_data.keys():
@@ -18,12 +18,8 @@ class QuoteKeeper:
                     continue  
 
     def add_quote(self, quote, member):
-        with open('quote_bank.json', 'r') as raw_quotes:
-            quote_dict = json.load(raw_quotes)
-            member_current_quotes = quote_dict[member]
-            member_current_quotes.append(quote)
-        with open('quote_bank.json', 'w') as raw_quotes:
-            json.dump(quote_dict, raw_quotes)
+        self.quoteKeeperDataManager.update_entry(member, "SavedQuotes", quote, increment=True)
+        print(f"[INFO] Quote Added From {member.name}.")
 
     def retrieve_quotes(self, member):
         with open('quote_bank.json') as raw_quotes:
