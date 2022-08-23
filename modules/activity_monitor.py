5a489d76094a7bce01b9a7dc1aefe3abb6039904
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-08-24 10:23:50 +1200

    Remaking activity_check method

--- modules/activity_monitor.py
+++ modules/activity_monitor.py
@@ -1,30 +1,24 @@
 import discord
-
+from sqlitedict import SqliteDict
 
 class ActivityMonitor:
-    def __init__(self, client):
-        try:
-            with open('activity_valut.json', 'r'):
-                print('[INFO] Existing Activity Data Detected - A New Activity File Will Not Be Generated')
-        except:
-            with open('activity_valut.json', 'x') as file:
-                print('[INFO] No Existing Activity Data Found - Generating New Activity File')
-                file.write('{}')
 
-        self.bot = client
-        self.activity_cache = {} 
+    CLASS_KEY = "ActivityHistory"
 
-    async def activity_check(self, user):
+    def __init__(self, guild_members, dataManager):
+        self.activityMonitorDataManager = dataManager
+        self.refresh_scores(guild_members)
+        self.activityMonitorDataManager.ensure_category(self.CLASS_KEY, {})
+
+    def full_activity_check(self, guild_members):
+        with SqliteDict("./data/memberData.db") as member_data:
+
+
         current_activities = user.activities
         spotify_activity = current_activities[1]  # For Future Implementation
         main_activity = current_activities[0]
 
-        if user not in self.activity_cache.keys():
-            self.activity_cache[user] = [{main_activity: 1}]
-        elif main_activity not in self.activity_cache[user][0]:
-            self.activity_cache[user][0][main_activity] = 1
-        else:
-            self.activity_cache[user][0][main_activity] += 1
+    
 
 
     
