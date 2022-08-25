b93b8eceb0bea61a723b2709934a91f835281862
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-08-26 10:21:58 +1200

    Activity_check working

--- modules/activity_monitor.py
+++ modules/activity_monitor.py
@@ -7,16 +7,14 @@ class ActivityMonitor:
 
     def __init__(self, guild_members, dataManager):
         self.activityMonitorDataManager = dataManager
-        self.refresh_scores(guild_members)
         self.activityMonitorDataManager.ensure_category(self.CLASS_KEY, {})
 
     def full_activity_check(self, guild_members): # Will be run every 5 minutes, so will assume user has been doing a given activity for that time.
         with SqliteDict("./data/memberData.db") as member_data:
             for member in guild_members:
-                spotify_activity = member.activities[1]  # For Future Implementation
-                main_activity = member.activities[0]
-
-                if main_activity != None:
+                print(f"{member.display_name} is currently doing {member.activity}")
+                if len(member.activities) != 0:
+                    main_activity = member.activities[0] 
                     current_history = self.activityMonitorDataManager.get_data(member, self.CLASS_KEY) # Dictionary
                     if current_history == None:
                         return
@@ -26,15 +24,6 @@ class ActivityMonitor:
                         current_time = current_history[main_activity.name]
                         current_time += 5
                         current_history[main_activity.name] = current_time
-
-    
-
-
-    
-
-
-
+        member_data.commit()
         
-
-        
-    
+        self.activityMonitorDataManager.dump_database()
